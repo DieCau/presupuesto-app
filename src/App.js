7aa0c8add3b96fcf60fab45c6d402b343b464afe
Diego Caucota <diegocaucota@gmail.com>
2022-01-23 09:14:12 -0300

    Se creo Componente Pregunta

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
+import React, { useState } from 'react';
+import Formulario from './components/Formulario';
+import Pregunta from "./components/Pregunta";
 
 function App() {
+
+    // Definir un state
+    const [ presupuesto, setPresupuesto ] = useState(0);
+    const [ restante, setRestante ] = useState(0);
+
+
   return (
-    <h1>Presupuesto</h1>
+    <div className="container">
+      <header>
+        <h1>Gasto Semanal</h1>
+
+        <div className="contenido-principal contenido">
+          <Pregunta 
+            setPresupuesto={setPresupuesto}
+            setRestante={setRestante}
+          />
+
+        <div className="row">
+          <div className="one-half column">
+            <Formulario />
+          </div> 
+
+          <div className="one-half column">
+            2
+          </div>  
+        </div>  
+        </div>
+
+      </header>
+    </div>
   );
 }
 
---
+++ src/components/Pregunta.js
@@ -0,0 +1,60 @@
+import React, { Fragment, useState } from 'react';
+import Error from './Error';
+
+const Pregunta = ({ setPresupuesto, setRestante }) => {
+
+  // Definir el State
+  const [ cantidad, setCantidad ] = useState(0);
+  const [ error, setError ] = useState(false);
+
+
+  // Funcion que lee el presupuesto
+  const definirPresupuesto = e => {
+    setCantidad( parseInt(e.target.value, 10));    
+  }
+  
+  // Submit para definir el prepuesto
+  const agregarPresupuesto = e => {
+    e.preventDefault();
+
+    // Validar que sea una cantidad valida
+    if( cantidad < 1 || isNaN(cantidad) ) {
+      setError(true);
+      return;
+    }
+
+    // Qué hacer si se pasa la validación
+    setError(false);
+    setPresupuesto(cantidad);
+    setRestante(cantidad);
+  }
+
+  return (
+      <Fragment>
+          <h2>Coloca tu Presupuesto</h2>
+          
+          { error ?  <Error  mensaje="El Prespuesto es Incorrecto" /> : null }
+
+          <form
+            onSubmit={ agregarPresupuesto }
+          >
+              <input
+                type="number"
+                className="u-full-width"
+                placeholder="Coloca tu presupuesto"   
+                onChange={ definirPresupuesto }           
+              />
+
+              <input 
+                type="submit" 
+                className='button-primary u-full-width'
+                value="Definir Presupuesto"
+              />
+
+          </form>
+
+      </Fragment>
+  );
+};
+
+export default Pregunta;
